27ebea13746d03f6d7a5b318aa51446ce49cee9f
pjhpoi66 <60897503+pjhpoi66@users.noreply.github.com>
2025-09-22 07:53:05 +0900

    CI/CD: TEST

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -60,7 +60,7 @@ function LoginPageContent() {
   return (
       <main className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg">
-          <h1 className="text-3xl font-bold text-center text-gray-800">LOGIN</h1>
+          <h1 className="text-3xl font-bold text-center text-gray-800">로그인</h1>
           <form onSubmit={handleLogin} className="space-y-6">
             <div>
               <label
