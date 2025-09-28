31a19b6e325a12b677f6bf6f5fb4ac65579e173c
pjhpoi66 <60897503+pjhpoi66@users.noreply.github.com>
2025-09-29 06:24:11 +0900

    CI/CD: Remove unused `cwd` configuration in PM2 ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
             name: 'gemini-frontend', // 앱 이름
             script: 'npm',
             args: 'start',
-            // cwd: '/home/pjhpoi66/gemini-chat/gemini-chat-front', // 필요시 주석 해제
             instances: 1,
             autorestart: true,
             watch: false,
